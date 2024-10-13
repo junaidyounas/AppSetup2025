7a4385a757fe03f6df8b0a9d25cec9a0493f56d0
Junaid <johnsriders@gmail.com>
2024-10-13 15:27:53 +0500

    env setup

--- app/screens/dashboard/index.tsx
+++ app/screens/dashboard/index.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { API_BASE } from '@env'
 
 type Props = {}
 
 const DashboardScreen = (props: Props) => {
   return (
     <View>
-      <Text>DashboardScreen</Text>
+      <Text>{API_BASE}</Text>
     </View>
   )
 }
